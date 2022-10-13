949cd889341b14ff7c4b6b4ab877a960e2c7aacc
Tiffany <tiff0081@zbc.dk>
2022-10-13 13:35:05 +0200

    lavet 4 forskellige grafer

--- public/js/graffer.js
+++ public/js/graffer.js
@@ -1,20 +1,13 @@
-//import Chart from "chart.js/auto";
-
 let graf1 = document.getElementById("time_altitude").getContext("2d");
 let graf2 = document.getElementById("temp_altitude").getContext("2d");
 let graf3 = document.getElementById("temp_pressure").getContext("2d");
-
-const labels = [];
-for (let i = 0; i < 12; ++i) {
-  labels.push(i.toString());
-}
+let graf4 = document.getElementById("pressure_altitude").getContext("2d");
 
 let time_altitude = new Chart(graf1, {
   // The type of chart we want to create
-  type: "line",
+  type: "scatter",
   // The data for our dataset
   data: {
-    //labels: labels,
     datasets: [
       {
         label: "Tid vs. Højde",
@@ -25,7 +18,12 @@ let time_altitude = new Chart(graf1, {
           { x: 25, y: 5 },
           { x: 30, y: -5 },
           { x: 40, y: 30 },
+          { x: 50, y: 40 },
+          { x: 60, y: 50 },
+          { x: 70, y: 70 },
         ],
+        showLine: true,
+        fill: false,
 
         backgroundColor: "rgb(255, 99, 132)",
         borderColor: "rgb(255, 99, 132)",
@@ -34,56 +32,249 @@ let time_altitude = new Chart(graf1, {
   },
   // Configuration options go here
   options: {
+    responsive: true,
+
+    title: {
+      display: true,
+      text: "Tiden i forhold til Højden",
+    },
     scales: {
-      yAxes: [
-        {
-          scaleLabel: {
-            display: true,
-            labelString: "Højde",
-          },
-        },
-      ],
       xAxes: [
         {
-          stacked: true,
           ticks: {
-            stepSize: 1,
+            stepSize: 5,
           },
           scaleLabel: {
             display: true,
-            labelString: "Tid",
+            labelString: "Tid [s]",
           },
         },
       ],
+      yAxes: [
+        {
+          ticks: {
+            stepSize: 5,
+          },
+          type: "linear",
+          scaleLabel: {
+            display: true,
+            labelString: "Højde [m]",
+          },
+        },
+      ],
+    },
+
+    elements: {
+      line: {
+        tension: 0,
+      },
     },
   },
 });
 
 let temp_altitude = new Chart(graf2, {
   // The type of chart we want to create
-  type: "",
+  type: "scatter",
   // The data for our dataset
-  data: {},
+  data: {
+    datasets: [
+      {
+        label: "Temperatur vs. Højde",
+        data: [
+          { x: 10, y: 20 },
+          { x: 15, y: 15 },
+          { x: 20, y: 10 },
+          { x: 25, y: 5 },
+          { x: 30, y: -5 },
+          { x: 40, y: 30 },
+          { x: 50, y: 40 },
+          { x: 60, y: 50 },
+          { x: 70, y: 70 },
+        ],
+        showLine: true,
+        fill: false,
+
+        backgroundColor: "rgb(0, 102, 255)",
+        borderColor: "rgb(0, 102, 255)",
+      },
+    ],
+  },
   // Configuration options go here
-  options: {},
+  options: {
+    responsive: true,
+
+    title: {
+      display: true,
+      text: "Temperaturen i forhold til Højden",
+    },
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            stepSize: 5,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Temperatur [℃]",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            stepSize: 5,
+          },
+          type: "linear",
+          scaleLabel: {
+            display: true,
+            labelString: "Højde [m]",
+          },
+        },
+      ],
+    },
+
+    elements: {
+      line: {
+        tension: 0,
+      },
+    },
+  },
 });
 
 let temp_pressure = new Chart(graf3, {
   // The type of chart we want to create
-  type: "",
+  type: "scatter",
   // The data for our dataset
-  data: {},
+  data: {
+    datasets: [
+      {
+        label: "Temperatur vs. Tryk",
+        data: [
+          { x: 10, y: 20 },
+          { x: 15, y: 15 },
+          { x: 20, y: 10 },
+          { x: 25, y: 5 },
+          { x: 30, y: -5 },
+          { x: 40, y: 30 },
+          { x: 50, y: 40 },
+          { x: 60, y: 50 },
+          { x: 70, y: 70 },
+        ],
+        showLine: true,
+        fill: false,
+
+        backgroundColor: "rgb(51, 204, 51)",
+        borderColor: "rgb(51, 204, 51)",
+      },
+    ],
+  },
   // Configuration options go here
-  options: {},
+  options: {
+    responsive: true,
+
+    title: {
+      display: true,
+      text: "Temperaturen i forhold til Trykket",
+    },
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            stepSize: 5,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Temperatur [℃]",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            stepSize: 5,
+          },
+          type: "linear",
+          scaleLabel: {
+            display: true,
+            labelString: "Tryk",
+          },
+        },
+      ],
+    },
+
+    elements: {
+      line: {
+        tension: 0,
+      },
+    },
+  },
 });
 
-/*
-https://stackoverflow.com/questions/39399264/in-chartjs-is-it-possible-to-change-the-line-style-between-different-points
+let pressure_altitude = new Chart(graf4, {
+  // The type of chart we want to create
+  type: "scatter",
+  // The data for our dataset
+  data: {
+    datasets: [
+      {
+        label: "Tryk vs. Højde",
+        data: [
+          { x: 10, y: 20 },
+          { x: 15, y: 15 },
+          { x: 20, y: 10 },
+          { x: 25, y: 5 },
+          { x: 30, y: -5 },
+          { x: 40, y: 30 },
+          { x: 50, y: 40 },
+          { x: 60, y: 50 },
+          { x: 70, y: 70 },
+        ],
+        showLine: true,
+        fill: false,
 
-const labels = [1, 2, 3, 4, 5, 6];
-const values = [12, 19, 3, 5, 2, 3];
-const data = labels.map((label, index) => ({ x: label, y: values[index]}));
+        backgroundColor: "rgb(153, 51, 255)",
+        borderColor: "rgb(153, 51, 255)",
+      },
+    ],
+  },
+  // Configuration options go here
+  options: {
+    responsive: true,
 
+    title: {
+      display: true,
+      text: "Trykket i forhold til højden",
+    },
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            stepSize: 5,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Tryk",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            stepSize: 5,
+          },
+          type: "linear",
+          scaleLabel: {
+            display: true,
+            labelString: "Højde [m]",
+          },
+        },
+      ],
+    },
 
-https://wpdatatables.com/chart-js-examples/
-*/
+    elements: {
+      line: {
+        tension: 0,
+      },
+    },
+  },
+});
