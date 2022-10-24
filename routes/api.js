91ae55a2af9c2febb603caba8676dba6a42ff067
Lauritz Thomsen <git@lauritzt.dk>
2022-10-24 08:31:39 +0200

    Update API

--- routes/api.js
+++ routes/api.js
@@ -8,20 +8,21 @@ router.post("/upload_data", async (req, res) => {
 
 
         // Request validation
-        if (body.timestamp === undefined) {
-            res.status(400).json({success: false});
+        if (body.datapoints === undefined) {
+            res.status(400).json({success: false, message: "Invalid request: No 'datapoints' key"});
             return;
         }
 
         res.json(
             {
                 success: false,
-                timestamp: body.timestamp,
+                message: "Not yet implemented",
             }
         );
     } catch (error) {
         res.status(500).json({
-            success: false
+            success: false,
+            message: "Internal server error"
         });
     }
 });
