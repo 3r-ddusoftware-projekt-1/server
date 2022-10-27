7503cd14d1d666d7fb68c9d54140526824a620a0
Tiffany <tiff0081@zbc.dk>
2022-10-27 11:21:40 +0200

    omdannet graffer til block

--- public/js/graffer.js
+++ public/js/graffer.js
@@ -1,237 +1,239 @@
-let graf1 = document.getElementById("time_altitude").getContext("2d");
-let graf2 = document.getElementById("temp_altitude").getContext("2d");
-let graf3 = document.getElementById("temp_pressure").getContext("2d");
-let graf4 = document.getElementById("pressure_altitude").getContext("2d");
-
-let time_altitude = new Chart(graf1, {
-  // The type of chart we want to create
-  type: "scatter",
-  // The data for our dataset
-  data: {
+// setup block
+const data1 = {
     datasets: [
-      {
-        label: "Tid vs. Højde",
-        // x=time, y=altitude
-        data: datasets1,
-        showLine: true,
-        fill: false,
+        {
+            label: "Tid vs. Højde",
+            // x=time, y=altitude
+            data: datasets1,
+            showLine: true,
+            fill: false,
 
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-      },
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+        },
     ],
-  },
-  // Configuration options go here
-  options: {
-    responsive: true,
+};
 
-    title: {
-      display: true,
-      text: "Tiden i forhold til Højden",
-    },
-    scales: {
-      xAxes: [
-        {
-          ticks: {
-            stepSize: 5,
-          },
-          scaleLabel: {
-            display: true,
-            labelString: "Tid [s]",
-          },
-        },
-      ],
-      yAxes: [
-        {
-          type: "linear",
-          scaleLabel: {
-            display: true,
-            labelString: "Højde [m]",
-          },
-        },
-      ],
-    },
-
-    elements: {
-      line: {
-        tension: 0,
-      },
-    },
-  },
-});
-
-let temp_altitude = new Chart(graf2, {
-  // The type of chart we want to create
-  type: "scatter",
-  // The data for our dataset
-  data: {
+const data2 = {
     datasets: [
-      {
-        label: "Temperatur vs. Højde",
-        // x=temperature, y=altitude
-        data: datasets2,
-        showLine: true,
-        fill: false,
+        {
+            label: "Temperatur vs. Højde",
+            // x=temperature, y=altitude
+            data: datasets2,
+            showLine: true,
+            fill: false,
 
-        backgroundColor: "rgb(0, 102, 255)",
-        borderColor: "rgb(0, 102, 255)",
-      },
+            backgroundColor: "rgb(0, 102, 255)",
+            borderColor: "rgb(0, 102, 255)",
+        },
     ],
-  },
-  // Configuration options go here
-  options: {
-    responsive: true,
+};
 
-    title: {
-      display: true,
-      text: "Temperaturen i forhold til Højden",
-    },
-    scales: {
-      xAxes: [
-        {
-          ticks: {
-            stepSize: 5,
-          },
-          scaleLabel: {
-            display: true,
-            labelString: "Temperatur [℃]",
-          },
-        },
-      ],
-      yAxes: [
-        {
-          type: "linear",
-          scaleLabel: {
-            display: true,
-            labelString: "Højde [m]",
-          },
-        },
-      ],
-    },
-
-    elements: {
-      line: {
-        tension: 0,
-      },
-    },
-  },
-});
-
-let temp_pressure = new Chart(graf3, {
-  // The type of chart we want to create
-  type: "scatter",
-  // The data for our dataset
-  data: {
+const data3 = {
     datasets: [
-      {
-        label: "Temperatur vs. Tryk",
-        // x=temperature, y=pressure
-        data: datasets3,
-        showLine: true,
-        fill: false,
+        {
+            label: "Temperatur vs. Tryk",
+            // x=temperature, y=pressure
+            data: datasets3,
+            showLine: true,
+            fill: false,
 
-        backgroundColor: "rgb(51, 204, 51)",
-        borderColor: "rgb(51, 204, 51)",
-      },
+            backgroundColor: "rgb(51, 204, 51)",
+            borderColor: "rgb(51, 204, 51)",
+        },
     ],
-  },
-  // Configuration options go here
-  options: {
-    responsive: true,
+};
 
-    title: {
-      display: true,
-      text: "Temperaturen i forhold til Trykket",
-    },
-    scales: {
-      xAxes: [
-        {
-          ticks: {
-            stepSize: 5,
-          },
-          scaleLabel: {
-            display: true,
-            labelString: "Temperatur [℃]",
-          },
-        },
-      ],
-      yAxes: [
-        {
-          type: "linear",
-          scaleLabel: {
-            display: true,
-            labelString: "Tryk",
-          },
-        },
-      ],
-    },
-
-    elements: {
-      line: {
-        tension: 0,
-      },
-    },
-  },
-});
-
-
-
-let pressure_altitude = new Chart(graf4, {
-  // The type of chart we want to create
-  type: "scatter",
-  // The data for our dataset
-  data: {
+const data4 = {
     datasets: [
-      {
-        label: "Tryk vs. Højde",
-        // x=pressure, y=altitude
-        data: datasets4,
+        {
+            label: "Tryk vs. Højde",
+            // x=pressure, y=altitude
+            data: datasets4,
 
-        showLine: true,
-        fill: false,
+            showLine: true,
+            fill: false,
 
-        backgroundColor: "rgb(153, 51, 255)",
-        borderColor: "rgb(153, 51, 255)",
-      },
+            backgroundColor: "rgb(153, 51, 255)",
+            borderColor: "rgb(153, 51, 255)",
+        },
     ],
-  },
-  // Configuration options go here
-  options: {
-    responsive: true,
+};
 
-    title: {
-      display: true,
-      text: "Trykket i forhold til højden",
-    },
-    scales: {
-      xAxes: [
-        {
-          ticks: {
-            stepSize: 5,
-          },
-          scaleLabel: {
+// config block
+const config1 = {
+    // The type of chart we want to create
+    type: "scatter",
+    // The data for our dataset
+    data: data1,
+    // Configuration options go here
+    options: {
+        responsive: true,
+        title: {
             display: true,
-            labelString: "Tryk",
-          },
+            text: "Tiden i forhold til Højden",
         },
-      ],
-      yAxes: [
-        {
-          type: "linear",
-          scaleLabel: {
+        scales: {
+            xAxes: [
+                {
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Tid [s]",
+                    },
+                },
+            ],
+            yAxes: [
+                {
+                    type: "linear",
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Højde [m]",
+                    },
+                },
+            ],
+        },
+        elements: {
+            line: {
+                tension: 0,
+            },
+        },
+    },
+};
+
+const config2 = {
+    // The type of chart we want to create
+    type: "scatter",
+    // The data for our dataset
+    data: data2,
+    // Configuration options go here
+    options: {
+        responsive: true,
+
+        title: {
             display: true,
-            labelString: "Højde [m]",
-          },
+            text: "Temperaturen i forhold til Højden",
         },
-      ],
+        scales: {
+            xAxes: [
+                {
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Temperatur [℃]",
+                    },
+                },
+            ],
+            yAxes: [
+                {
+                    type: "linear",
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Højde [m]",
+                    },
+                },
+            ],
+        },
+
+        elements: {
+            line: {
+                tension: 0,
+            },
+        },
     },
+};
+const config3 = {
+    // The type of chart we want to create
+    type: "scatter",
+    // The data for our dataset
+    data: data3,
+    // Configuration options go here
+    options: {
+        responsive: true,
 
-    elements: {
-      line: {
-        tension: 0,
-      },
+        title: {
+            display: true,
+            text: "Temperaturen i forhold til Trykket",
+        },
+        scales: {
+            xAxes: [
+                {
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Temperatur [℃]",
+                    },
+                },
+            ],
+            yAxes: [
+                {
+                    type: "linear",
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Tryk",
+                    },
+                },
+            ],
+        },
+
+        elements: {
+            line: {
+                tension: 0,
+            },
+        },
     },
-  },
-});
+};
+const config4 = {
+    // The type of chart we want to create
+    type: "scatter",
+    // The data for our dataset
+    data: data4,
+    // Configuration options go here
+    options: {
+        responsive: true,
 
+        title: {
+            display: true,
+            text: "Trykket i forhold til højden",
+        },
+        scales: {
+            xAxes: [
+                {
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Tryk",
+                    },
+                },
+            ],
+            yAxes: [
+                {
+                    type: "linear",
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Højde [m]",
+                    },
+                },
+            ],
+        },
 
+        elements: {
+            line: {
+                tension: 0,
+            },
+        },
+    },
+};
+// render init block
+const time_altitude = new Chart(
+    document.getElementById("time_altitude"),
+    config1
+);
+const temp_altitude = new Chart(
+    document.getElementById("temp_altitude"),
+    config2
+);
+const temp_pressure = new Chart(
+    document.getElementById("temp_pressure"),
+    config3
+);
+const pressure_altitude = new Chart(
+    document.getElementById("pressure_altitude"),
+    config4
+);
